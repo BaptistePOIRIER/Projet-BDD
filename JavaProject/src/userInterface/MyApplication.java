f20231ae835fd17b27e87991d626ef3ce0db07c7
Theticman <75702738+BaptistePOIRIER@users.noreply.github.com>
2021-05-15 12:55:03 +0200

    UserInterface test

---
+++ JavaProject/src/userInterface/MyApplication.java
@@ -0,0 +1,79 @@
+package userInterface;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class MyApplication {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyApplication window = new MyApplication();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MyApplication() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 501, 350);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Tapez votre nom");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(55, 29, 185, 41);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(237, 32, 131, 41);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel(".................");
+		lblNewLabel_1.setBounds(237, 96, 131, 41);
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Action");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String s = textField.getText();
+				lblNewLabel_1.setText("Bienvenue : " + s);
+			}
+		});
+		btnNewButton.setBounds(247, 147, 171, 41);
+		panel.add(btnNewButton);
+	}
+}
